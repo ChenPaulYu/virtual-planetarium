d7bb560c9876ef817e3974e0ac791d931ae675b0
PaulChen <bernie40916@gmail.com>
2019-03-21 15:23:19 +0800

    update3

--- js/control.js
+++ js/control.js
@@ -1,16 +1,18 @@
-if (window.DeviceOrientationEvent) {
-    $(window).on('deviceorientation', function (event) {
-        var a = document.getElementById('alpha'),
-            b = document.getElementById('beta'),
-            g = document.getElementById('gamma'),
-            alpha = event.alpha,
-            beta = event.beta,
-            gamma = event.gamma;
+    if (window.DeviceOrientationEvent) {
 
-        a.innerHTML = Math.round(alpha);
-        b.innerHTML = Math.round(beta);
-        g.innerHTML = Math.round(gamma);
-    }, false)
-} else {
-    document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
-}
+        window.addEventListener('deviceorientation', function (event) {
+            var a = document.getElementById('alpha'),
+                b = document.getElementById('beta'),
+                g = document.getElementById('gamma'),
+                alpha = event.alpha,
+                beta = event.beta,
+                gamma = event.gamma;
+
+            a.innerHTML = Math.round(alpha);
+            b.innerHTML = Math.round(beta);
+            g.innerHTML = Math.round(gamma);
+
+        }, false);
+    } else {
+        document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
+    }
