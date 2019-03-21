5023b3aedc271f726246f6ba14606e15b2e76f44
PaulChen <bernie40916@gmail.com>
2019-03-21 15:14:05 +0800

    extract

--- js/control.js
+++ js/control.js
@@ -1,31 +1,16 @@
-if(window.DeviceOrientationEvent) {
+if (window.DeviceOrientationEvent) {
+    $(window).on('deviceorientation', function (event) {
+        var a = document.getElementById('alpha'),
+            b = document.getElementById('beta'),
+            g = document.getElementById('gamma'),
+            alpha = event.alpha,
+            beta = event.beta,
+            gamma = event.gamma;
 
-    // $(window).on('deviceorientation',function(event){
-    //   var a = document.getElementById('alpha'),
-    //       b = document.getElementById('beta'),
-    //       g = document.getElementById('gamma'),
-    //       alpha = event.alpha,
-    //       beta = event.beta,
-    //       gamma = event.gamma;
-        
-    //     a.innerHTML = Math.round(alpha);
-    //     b.innerHTML = Math.round(beta);
-    //     g.innerHTML = Math.round(gamma);
-
-    //   }, false);
-
-	window.addEventListener('deviceorientation', function(event) {
-      var a = document.getElementById('alpha'),
-      	  b = document.getElementById('beta'),
-      	  g = document.getElementById('gamma'),
-      	  alpha = event.alpha,
-          beta = event.beta,
-          gamma = event.gamma;
-          a.innerHTML = Math.round(alpha);
-          b.innerHTML = Math.round(beta);
-          g.innerHTML = Math.round(gamma);
-          
-		}, false);
-  }else{
-  	document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
-  }
+        a.innerHTML = Math.round(alpha);
+        b.innerHTML = Math.round(beta);
+        g.innerHTML = Math.round(gamma);
+    }, false)
+} else {
+    document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
+}
