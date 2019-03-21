b4994c07718b1f13f7466f35f070951fd6ffe744
PaulChen <bernie40916@gmail.com>
2019-03-21 17:08:47 +0800

    trying

--- js/control.js
+++ js/control.js
@@ -1,44 +1,65 @@
-if (window.DeviceOrientationEvent) {
+var orientationData = {
+    gamma: 0,
+    beta: 0,
+    alpha: 0
+}
+var motionData = {
+    z: 0,
+    y: 0,
+    x: 0,
+    zr: 0
+}
+var values = {
+    p: 0,
+    v: 0,
+    a: 0,
+    lastP: 0,
+    lastV: 0,
+    xyzA: 0
+}
+var countHit = 0;
+let lastTriggerTime = 0
 
-    window.addEventListener('deviceorientation', function (event) {
-        var a = document.getElementById('alpha');
-        var b = document.getElementById('beta');
-        var g = document.getElementById('gamma');
-        var alpha = event.alpha;
-        var beta = event.beta;
-        var gamma = event.gamma;
 
-            
-        
-        a.innerHTML = Math.round(alpha);
-        b.innerHTML = Math.round(beta);
-        g.innerHTML = Math.round(gamma);
-
-    }, false);
-
-    window.addEventListener('devicemotion', function (event) {
-
-        var ax = document.getElementById('ax');
-        var ay = document.getElementById('ay');
-        var az = document.getElementById('az');
-        var gesture = document.getElementById('gesture');
-
-        var ax_value = event.acceleration.x;
-        var ay_value = event.acceleration.y;
-        var az_value = event.acceleration.z;
-
-        ax.innerHTML = Math.round(ax_value);
-        ay.innerHTML = Math.round(ay_value);
-        az.innerHTML = Math.round(az_value);
-
-        if(ax_value > 0) {
-            gesture.innerHTML = " > 0 "
-        } else if (ax_value < 0) {
-            gesture.innerHTML = " < 0 "
+function orientation(event) {
+    if (event.gamma) {
+        orientationData = {
+            gamma: event.gamma || 0,
+            beta : event.beta  || 0,
+            alpha: event.alpha || 0
         }
+    }
+}
 
-    }, false);
+function handleMotion(event) {
+    motionData = {
+        z: event.acceleration.z || 0,
+        y: event.acceleration.y || 0,
+        x: event.acceleration.x || 0
+    }
+}
 
+var count = 0
+
+function main() {
+    values.lastP = values.p
+    values.lastV = values.v
+    values.lastA = values.a
+
+    values.p = orientationData.beta
+    values.xyzA = Math.sqrt(Math.pow(motionData.x, 2) + Math.pow(motionData.y, 2) + Math.pow(motionData.z, 2));
+    values.v = values.p - values.lastP
+    values.a = values.v - values.lastV
+    $('#xyzA').text(count)
+}
+
+
+
+if (window.DeviceOrientationEvent && window.DeviceMotionEvent) {
+    window.addEventListener('deviceorientation', orientation, false);
+    window.addEventListener('devicemotion', handleMotion, false);
 } else {
     document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
 }
+
+setInterval(main,100)
