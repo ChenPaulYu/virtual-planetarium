e56f4dc1066b03ffad5b24dd03de084123c60737
PaulChen <bernie40916@gmail.com>
2019-03-21 15:29:59 +0800

    adding accerelation

--- js/control.js
+++ js/control.js
@@ -1,16 +1,25 @@
     if (window.DeviceOrientationEvent) {
-
         window.addEventListener('deviceorientation', function (event) {
-            var a = document.getElementById('alpha'),
-                b = document.getElementById('beta'),
-                g = document.getElementById('gamma'),
+            var a  = document.getElementById('alpha'),
+                b  = document.getElementById('beta'),
+                g  = document.getElementById('gamma'),
+                ax = document.getElementById('ax'),
+                ay = document.getElementById('ay'),
+                az = document.getElementById('az'),
                 alpha = event.alpha,
                 beta = event.beta,
                 gamma = event.gamma;
-
+                ax_value = event.acceleration.x
+                ay_value = event.acceleration.y
+                az_value = event.acceleration.z
+        
             a.innerHTML = Math.round(alpha);
             b.innerHTML = Math.round(beta);
             g.innerHTML = Math.round(gamma);
+
+            ax.innerHTML = Math.round(ax_value);
+            ay.innerHTML = Math.round(ay_value);
+            az.innerHTML = Math.round(az_value);
 
         }, false);
     } else {
