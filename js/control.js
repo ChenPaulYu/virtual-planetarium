8c4266fa457d1a57619c10409fa6f48b91e3d757
PaulChen <bernie40916@gmail.com>
2019-03-21 15:34:00 +0800

    adding motion

--- js/control.js
+++ js/control.js
@@ -3,25 +3,31 @@
             var a  = document.getElementById('alpha'),
                 b  = document.getElementById('beta'),
                 g  = document.getElementById('gamma'),
-                ax = document.getElementById('ax'),
-                ay = document.getElementById('ay'),
-                az = document.getElementById('az'),
                 alpha = event.alpha,
                 beta = event.beta,
                 gamma = event.gamma;
-                ax_value = event.acceleration.x
-                ay_value = event.acceleration.y
-                az_value = event.acceleration.z
         
             a.innerHTML = Math.round(alpha);
             b.innerHTML = Math.round(beta);
             g.innerHTML = Math.round(gamma);
+
+        }, false);
+
+        window.addEventListener('devicemotion', function (event) {
+            var ax = document.getElementById('ax'),
+                ay = document.getElementById('ay'),
+                az = document.getElementById('az'),
+
+            ax_value = event.acceleration.x
+            ay_value = event.acceleration.y
+            az_value = event.acceleration.z
 
             ax.innerHTML = Math.round(ax_value);
             ay.innerHTML = Math.round(ay_value);
             az.innerHTML = Math.round(az_value);
 
         }, false);
+        
     } else {
         document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
     }
