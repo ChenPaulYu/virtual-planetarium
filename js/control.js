0da4e74274f142249a1a324e02f1fa9bbe9a9217
PaulChen <bernie40916@gmail.com>
2019-03-21 14:51:27 +0800

    delete w in control.js

--- js/control.js
+++ js/control.js
@@ -1,11 +1,26 @@
 if(window.DeviceOrientationEvent) {
+
+    // $(window).on('deviceorientation',function(event){
+    //   var a = document.getElementById('alpha'),
+    //       b = document.getElementById('beta'),
+    //       g = document.getElementById('gamma'),
+    //       alpha = event.alpha,
+    //       beta = event.beta,
+    //       gamma = event.gamma;
+        
+    //     a.innerHTML = Math.round(alpha);
+    //     b.innerHTML = Math.round(beta);
+    //     g.innerHTML = Math.round(gamma);
+
+    //   }, false);
+
 	window.addEventListener('deviceorientation', function(event) {
       var a = document.getElementById('alpha'),
       	  b = document.getElementById('beta'),
       	  g = document.getElementById('gamma'),
       	  alpha = event.alpha,
           beta = event.beta,
-          gamma = event.gamma;w
+          gamma = event.gamma;
           a.innerHTML = Math.round(alpha);
           b.innerHTML = Math.round(beta);
           g.innerHTML = Math.round(gamma);
