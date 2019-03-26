c267bda1e3d9686efba50d8bb2841417dc642eab
PaulChen <bernie40916@gmail.com>
2019-03-27 02:20:16 +0800

    edit a lot of new things

--- js/index.js
+++ js/index.js
@@ -37,9 +37,9 @@
         "dec": -5.0791694,
         "width": 263,
         "height": 587.66,
-        "offsetX": 10,
-        "offsetY": 6,
-        "scaleX": 0,
+        "offsetX": 15,
+        "offsetY": 10,
+        "scaleX": 1,
         "scaleY": 0,
         "intro": "sJITcyf.png"
       }, {
@@ -48,9 +48,9 @@
         "dec": 22.0145000,
         "width": 263,
         "height": 536.66,
-        "offsetX": 10,
-        "offsetY": 6,
-        "scaleX": 0,
+        "offsetX": 15,
+        "offsetY": 10,
+        "scaleX": 1,
         "scaleY": 0,
         "intro": "hayBVBB.png"
       }, {
@@ -59,8 +59,8 @@
         "dec": 55.0190889,
         "width": 263,
         "height": 761.33,
-        "offsetX": 10,
-        "offsetY": 6,
+        "offsetX": -35,
+        "offsetY": 10,
         "scaleX": 0,
         "scaleY": 0,
         "intro": "z1BY63E.png"
@@ -70,20 +70,20 @@
         "dec": 30,
         "width": 263,
         "height": 670,
-        "offsetX": 10,
-        "offsetY": 6,
-        "scaleX": 0,
+        "offsetX": 25,
+        "offsetY": 10,
+        "scaleX": 1,
         "scaleY": 0,
         "intro": "aQUzTED.png"
       }, {
         "name": "Lyr",
         "ra": 294.9987917,
-        "dec": 40.8651694,
+        "dec": 35.8651694,
         "width": 263,
         "height": 764.33,
-        "offsetX": 10,
-        "offsetY": 6,
-        "scaleX": 0,
+        "offsetX": -8,
+        "offsetY": 5,
+        "scaleX": 1,
         "scaleY": 0,
         "intro": "PypCwTG.png"
       }, {
@@ -92,9 +92,9 @@
         "dec": 20.8223806,
         "width": 263,
         "height": 610.3,
-        "offsetX": 10,
+        "offsetX": 18,
         "offsetY": 6,
-        "scaleX": 0,
+        "scaleX": 1,
         "scaleY": 0,
         "intro": "7B00n3L.png"
       }, {
@@ -103,9 +103,9 @@
         "dec": 80,
         "width": 263,
         "height": 761.33,
-        "offsetX": 10,
+        "offsetX": 100,
         "offsetY": 6,
-        "scaleX": 0,
+        "scaleX": 1,
         "scaleY": 0,
         "intro": "z1BY63E.png"
       }, {
@@ -114,9 +114,9 @@
         "dec": 30.6599417,
         "width": 263,
         "height": 384.33,
-        "offsetX": 10,
+        "offsetX": 15,
         "offsetY": 6,
-        "scaleX": 0,
+        "scaleX": 1,
         "scaleY": 0,
         "intro": "OU12G4N.png"
       }, {
@@ -125,9 +125,9 @@
         "dec": 34.03,
         "width": 263,
         "height": 395,
-        "offsetX": 10,
-        "offsetY": 6,
-        "scaleX": 0,
+        "offsetX": 15,
+        "offsetY": 10,
+        "scaleX": 1,
         "scaleY": 0,
         "intro": "iZ6EBk6.png"
       }
@@ -136,6 +136,8 @@
 
 var meteor_c = []; 
 var meteor_l = [];
+var meteorites = []
+var stars = []
 const synth = new Tone.MembraneSynth().toMaster();
 
 var config = {
@@ -187,41 +189,63 @@ S(document).ready(function () {
         });
     }
 
+    var canv = document.createElement('canvas');
+    canv.id = 'buffer';
+    document.getElementById('starmap_inner').appendChild(canv);
 
         
+    setInterval(() => {
 
-        
-        var refreshid = setInterval(() => {
-            if (!planetarium.animate) {
-                var w = $(document).width();
-                var h = $(document).height();
-                planetarium.draw()
-                for(var i = 0;i< meteor_c.length;i++) {
-                    drawC(meteor_c[i])
-                    if (meteor_c[i].x > w || meteor_c[i].y > h || meteor_c[i].x < 0 || meteor_c[i].y < 0) {
-                        meteor_c.splice(i, 1)
-                    }
-                }
-                for (var i = 0; i < meteor_l.length; i++) {
-                    drawL(meteor_l[i])
-                    if (meteor_l[i].x > w || meteor_l[i].y > h || meteor_l[i].x < 0 || meteor_l[i].y < 0) {
-                        synth.triggerAttackRelease("C2", "4n");
-                        meteor_l.splice(i, 1)
-                    }
+        if (!planetarium.animate) {
+            var w = $(document).width();
+            var h = $(document).height();
+            planetarium.draw()
+            for(var i = 0;i< meteor_c.length;i++) {
+                drawC(meteor_c[i])
+                if (meteor_c[i].x > w || meteor_c[i].y > h || meteor_c[i].x < 0 || meteor_c[i].y < 0) {
+                    meteor_c.splice(i, 1)
                 }
             }
 
-        }, 10);   
-        var refreshid = setInterval(() => {
-            addstarL(1)
-        }, 2000);
-         setInterval(() => {
-            addstarC(10)
+            for (var i = 0; i < meteor_l.length; i++) {
+                drawL(meteor_l[i])
+                if (meteor_l[i].x > w || meteor_l[i].y > h || meteor_l[i].x < 0 || meteor_l[i].y < 0) {
+                    synth.triggerAttackRelease("C2", "4n");
+                    meteor_l.splice(i, 1)
+                }
+            }
+
+            for (let i = 0; i < meteorites.length; i++) {
+                meteorites[i].update();
+                if (meteorites[i].x > w || meteorites[i].y > h || meteorites[i].x < 0 || meteorites[i].y < 0) {
+                    meteorites.splice(i, 1)
+                }
+            }
+            
+            for (let i = 0; i < stars.length; i++) {
+                stars[i].update();
+            }
+               
+        }
+
+    }, 10);   
+        addS(300)
+        var count = 0
+        setInterval(() => {
+            if(meteor_c.length < 50) {
+                addstarC(10)    
+            }
+            
+            
+            count++
+            if(count%2 == 0 ) {
+                // addstarL(1)
+                addM(5);
+            }
         }, 1000);
 
         function addstarL(c) {
             var w = document.body.offsetWidth;
-            var h = document.body.offsetHeight;
             var count_l = Math.round(Math.random() * c)
 
             for (var i = 0; i < count_l; i++) {
@@ -301,73 +325,111 @@ S(document).ready(function () {
             ctx.fill();
 
         }
+
+
         
-
-
-
-
-    $('#next').on('click',function(){
-        
-        if(target < 11) { 
-            
-            console.log(target)
-            planetarium.toggleInfoBox(target, false)
-            target = target + 1
-            $('#target').text(target)
-            planetarium.target = target
-            planetarium.panTo(dec_ra[planetarium.target].ra, dec_ra[planetarium.target].dec, 0)
-            setTimeout(function(){
-                console.log('trigger')
-                if(planetarium.fov == 35) {
-                    planetarium.toggleInfoBox(target, true)
-                }
-                
-            },1000)
+        function addM(c) {
+            var count = Math.round(Math.random()* c)
+            for (let i = 0; i < count; i++) {
+                var c = document.getElementById("starmap_inner");
+                var ctx = c.getContext("2d");
+                const m = new Meteorite(ctx)
+                meteorites.push(m);
+            }
         }
-    })
 
-    $('#last').on('click', function () {
-        // clearInterval(refreshid)
-        planetarium.draw()
-        if (target > 0) {
+        function addS(c) {
             
-            console.log(target)
+            var count = c/1
+            for (var i = 0; i < count; i++) {
+                var c = document.getElementById("starmap_inner");
+                var ctx = c.getContext("2d");
+                const s = new Star(ctx)
+                stars.push(s);
+            }
+        }
+
+
+
+
+    $('#last').on('click',function(){
+        if (target > 0 && !planetarium.animate) {
             planetarium.toggleInfoBox(target, false)
+            synth.triggerAttackRelease("C2", "8n");
             target = target - 1
             planetarium.target = target
-            $('#target').text(target)
+            if (planetarium.fov == 35) {
+                planetarium.alpha = 0
+            }
             planetarium.panTo(dec_ra[planetarium.target].ra, dec_ra[planetarium.target].dec, 0)
-            setTimeout(function () {
-                if (planetarium.fov == 35) {
-                    planetarium.toggleInfoBox(target, true)
-                }
-
-            }, 1000)
+        } else {
+            synth.triggerAttackRelease("C5", "8n");
         }
+        $('#target').text(target)
+    })
+
+    $('#next').on('click', function () {
+        if (target < 11 && !planetarium.animate) {
+            planetarium.toggleInfoBox(target, false)
+            synth.triggerAttackRelease("C2", "2n");
+            target = target + 1
+            planetarium.target = target
+            if (planetarium.fov == 35) {
+                planetarium.alpha = 0
+            }
+                planetarium.panTo(dec_ra[planetarium.target].ra, dec_ra[planetarium.target].dec, 0)
+            } else {
+                synth.triggerAttackRelease("C5", "8n");
+            }
+            console.log('left')
+            $('#target').text(target)
     })
 
     $('#zoomin').on('click', function () {
-        var refreshId = setInterval(function(){
-            if (planetarium.fov <= 35) {
-                planetarium.toggleInfoBox(target,true)
-                clearInterval(refreshId);
-            }
-            planetarium.fov -= 0.1
-            planetarium.changeFOV(0).draw()
-        },5)
+        if (planetarium.fov > 35) {
+            planetarium.alpha = 0
+            synth.triggerAttackRelease("C2", "2n");
+            var refreshId = setInterval(function () {
+                planetarium.animate = true;
+                use = true
+                if (planetarium.fov <= 35) {
+                    use = false
+                    planetarium.animate = false;
+                    clearInterval(refreshId);
+                } else {
+                    planetarium.fov -= 0.1
+                    planetarium.changeFOV(0).draw()
+                }
+
+            }, 5)
+        } else {
+            synth.triggerAttackRelease("C5", "8n");
+        }
+        $('#target').text(target)
 
     })
 
     $('#zoomout').on('click', function () {
-         planetarium.toggleInfoBox(target, false)
-        var refreshId = setInterval(function () {
-            if (planetarium.fov >= 60) {
-                clearInterval(refreshId);
-            }
-            planetarium.fov += 0.1
-            planetarium.changeFOV(0).draw()
-        }, 5)
+        synth.triggerAttackRelease("C2", "2n");
+        if (planetarium.fov < 60) {
+            use = true
+            var refreshId = setInterval(function () {
+                if (planetarium.fov >= 60) {
+                    planetarium.animate = false;
+                    use = false
+                    clearInterval(refreshId);
+                } else {
+                    planetarium.animate = true;
+                    planetarium.toggleInfoBox(target, false)
+                    planetarium.fov += 0.1
+                    planetarium.changeFOV(0).draw()
+                }
 
+            }, 5)
+        } else {
+            synth.triggerAttackRelease("C5", "8n");
+        }
+        $('#target').text(target)
     })
 
     
@@ -383,17 +445,16 @@ S(document).ready(function () {
 
         if (newTime - lastTime > 500) {
             if (data == 'right') {
-                if (target > 0) {
+
+                if (target > 0 && !planetarium.animate) {
                     planetarium.toggleInfoBox(target, false)
                     synth.triggerAttackRelease("C2", "8n");
                     target = target - 1
                     planetarium.target = target
+                    if (planetarium.fov == 35) {
+                        planetarium.alpha = 0
+                    }
                     planetarium.panTo(dec_ra[planetarium.target].ra, dec_ra[planetarium.target].dec, 0)
-                    setTimeout(function () {
-                        if (planetarium.fov == 35) {
-                            planetarium.toggleInfoBox(target, true)
-                        }
-                    }, 1000)
                 }else {
                     synth.triggerAttackRelease("C5", "8n");
                 }
@@ -401,19 +462,17 @@ S(document).ready(function () {
                 $('#target').text(target)
                 console.log('right')
 
-            } else if (data == 'left') {
-                if (target < 11) {
+            } else if (data == 'left' ) {
+                if (target < 11 && !planetarium.animate) {
+                    console.log('trigger')
                     planetarium.toggleInfoBox(target, false)
                     synth.triggerAttackRelease("C2", "2n");
                     target = target + 1
                     planetarium.target = target
+                    if(planetarium.fov == 35) {
+                        planetarium.alpha = 0
+                    }
                     planetarium.panTo(dec_ra[planetarium.target].ra, dec_ra[planetarium.target].dec, 0)
-                    setTimeout(function () {
-                        if (planetarium.fov == 35) {
-                            planetarium.toggleInfoBox(target, true)
-                        }
-
-                    }, 1000)
                 }else {
                     synth.triggerAttackRelease("C5", "8n");
                 }
@@ -423,16 +482,20 @@ S(document).ready(function () {
 
             } else if (data == 'front' && !use) {
                 if (planetarium.fov > 35) {
+                    planetarium.alpha = 0
+                    synth.triggerAttackRelease("C2", "2n");
                     var refreshId = setInterval(function () {
+                        planetarium.animate = true;
                         use = true
                         if (planetarium.fov <= 35) {
                             use = false
+                            planetarium.animate = false;
                             clearInterval(refreshId);
-                            planetarium.toggleInfoBox(target, true)
+                        }else {
+                            planetarium.fov -= 0.1
+                            planetarium.changeFOV(0).draw()
                         }
-                        planetarium.fov -= 0.1
-                        synth.triggerAttackRelease("C2", "2n");
-                        planetarium.changeFOV(0).draw()
+
                     }, 5)
                 } else {
                     synth.triggerAttackRelease("C5", "8n");
@@ -440,17 +503,21 @@ S(document).ready(function () {
                 $('#gesture').text('front')
                 $('#target').text(target)
             } else if (data == 'back' && !use) {
-                planetarium.toggleInfoBox(target, false)
+                synth.triggerAttackRelease("C2", "2n");
                 if (planetarium.fov < 60) {
                     use = true
                     var refreshId = setInterval(function () {
                         if (planetarium.fov >= 60) {
+                            planetarium.animate = false;
                             use = false
                             clearInterval(refreshId);
+                        }else {
+                            planetarium.animate = true;
+                            planetarium.toggleInfoBox(target, false)
+                            planetarium.fov += 0.1
+                            planetarium.changeFOV(0).draw()
                         }
-                        planetarium.fov += 0.1
-                        synth.triggerAttackRelease("C2", "2n");
-                        planetarium.changeFOV(0).draw()
+
                     }, 5)
                 } else {
                     synth.triggerAttackRelease("C5", "8n");
@@ -530,3 +597,98 @@ S(document).ready(function () {
     // });
 
 });
+
+const maxSize = 3
+const maxSpeed = 10
+const minSpeed = 5
+const color = '#FFFFFF'
+
+class Star {
+    constructor(context) {
+        var w = $(document).width();
+        var h = $(document).height();
+        this.x = Math.random() * w;
+        this.y = Math.random() * h;
+        this.color = color
+        this.context = context;
+        this.radius = Math.random() * 1;
+        this.originalRadius = this.radius;
+        this.maxDuration = Math.abs(Math.random() * 200) + 50;
+        this.timing = 0
+        this.expanding = true
+    }
+
+
+    update(t) {
+        if (!this.expanding) {
+            this.radius -= this.originalRadius / this.maxDuration
+            this.timing--;
+        } else {
+            this.radius += this.originalRadius / this.maxDuration
+            this.timing++;
+        }
+
+        if (this.timing === 0) {
+            this.expanding = true
+        } else if (this.timing >= this.maxDuration) {
+            this.expanding = false
+        }
+
+        this.draw()
+    }
+
+    draw(x = this.x, y = this.y, radius = this.radius, color = this.color) {
+        const c = this.context;
+        c.save();
+        c.beginPath();
+        c.arc(x, y, radius, 0, Math.PI * 2, false);
+        c.shadowColor = color;
+        c.shadowBlur = 10;
+        c.shadowOffsetX = 0;
+        c.shadowOffsetY = 0;
+        c.fillStyle = color;
+        c.fill();
+        c.closePath();
+        c.restore();
+    }
+
+
+
+}
+
+class Meteorite extends Star {
+
+    constructor(context) {
+        super();
+        this.context = context
+        this.originalPosition = {
+            x: this.x,
+            y: this.y
+        }
+        this.radius = Math.random() * maxSize;
+        this.speed = Math.random() * maxSpeed + minSpeed;
+        this.trailLength = Math.floor(Math.random() * 25 + 15)
+    }
+
+    update() {
+        if (this.x > window.innerWidth + this.radius + this.trailLength) {
+            this.x = -this.radius * 10;
+        }
+
+        if (this.y > window.innerHeight + this.radius + this.trailLength) {
+            this.y = -this.radius * 10;
+        }
+
+        this.draw(this.x, this.y, this.radius * 1.5, 'rgba(255, 255, 255, 0.2')
+        for (let i = 0; i <= this.trailLength; i++) {
+            const color = 'rgba(255, 255, 255, ' + Math.abs(1 - i / this.trailLength) + ')'
+            this.draw(this.x - i * 2, this.y - i * 2, this.radius - (i * this.radius / (this.trailLength * 1.7)), color)
+        }
+        this.speed += 0.01
+        this.x += this.speed;
+        this.y += this.speed;
+
+    }
+}
+
+
